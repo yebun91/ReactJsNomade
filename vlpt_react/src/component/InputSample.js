ea275704e19d63c0cdb2d63d40f001773a6615c2
yjchoi <yjchoi@unisemiot.com>
2022-05-16 17:46:00 +0900

    no message

--- vlpt_react/src/component/InputSample.js
+++ vlpt_react/src/component/InputSample.js
@@ -1,16 +1,19 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 
 function InputSample() {
   const [inputs, setInputs] = useState({
     name: "",
     nickname: "",
   });
-  const { name, nickname } = inputs;
+  const nameInput = useRef();
+
+  const { name, nickname } = inputs; // 비구조화 할당을 통해 값 추출
+
   const onChange = (e) => {
-    const { value, name } = e.target;
+    const { value, name } = e.target; // 우선 e.target 에서 name 과 value 를 추출
     setInputs({
-      ...inputs,
-      [name]: value,
+      ...inputs, // 기존의 input 객체를 복사한 뒤
+      [name]: value, // name 키를 가진 값을 value 로 설정
     });
   };
 
@@ -19,14 +22,21 @@ function InputSample() {
       name: "",
       nickname: "",
     });
+    nameInput.current.focus();
   };
 
   return (
     <div>
-      <input placeholder="이름" name="name" onChange={onChange} value={name} />
       <input
-        placeholder="닉네임"
+        name="name"
+        placeholder="이름"
+        onChange={onChange}
+        value={name}
+        ref={nameInput}
+      />
+      <input
         name="nickname"
+        placeholder="닉네임"
         onChange={onChange}
         value={nickname}
       />
